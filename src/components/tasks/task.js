e693184add70d904dcb1e28e09ed9a8886ef3874
Samuel Caçador <samuelmoreira27@gmail.com>
2019-03-31 19:33:30 +0100

    Redo task line, pretty now :)

--- src/components/tasks/task.js
+++ src/components/tasks/task.js
@@ -6,19 +6,7 @@ class Task extends Component {
     constructor(props) {
         super(props);
         this.toggleTask = this.toggleTask.bind(this);
-        this.cardClick = this.cardClick.bind(this);
         this.removeTask = this.removeTask.bind(this);
-    }
-
-    cardClick(e) {
-        /** TODO: Separate card from remove button and try to remove this ugly condition. */
-        // if (e.target.className === 'custom-control-input' || 
-        //     e.target.className === 'custom-control-label' ||
-        //     e.target.parentNode.parentNode.className === 'remove-task'
-        // ) 
-        //     return;
-
-        this.toggleTask();
     }
 
     toggleTask() {
@@ -29,35 +17,21 @@ class Task extends Component {
         this.props.removeHandler(this.props.id);
     }
 
-    getTaskDescription() {
-        if(this.props.pending === false) {
-            return <span><s>{this.props.name}</s></span>;
-        }
-
-        return <span>{this.props.name}</span>;
-    }
-
-    /**
-     * TODO: CSS for this
-     * Make it pretty please :)
-     */
     render() {
         return (
             <div>
-                <div className="card" onClick={this.cardClick}>
+                <div className="card">
                     <div className="card-body">
-                        <div className="custom-control custom-switch">
-                            <input type="checkbox" className="custom-control-input" id={this.props.htmlId} checked={!this.props.pending} />
+                        <div className="custom-control custom-checkbox">
+                            <input type="checkbox" className="custom-control-input" id={this.props.htmlId} checked={!this.props.pending} onChange={this.toggleTask} />
                             <label className="custom-control-label" htmlFor={this.props.htmlId}>
-                                {this.getTaskDescription()}
+                                <span>{this.props.name}</span>
                             </label>
+                            <span className="remove-task" onClick={this.removeTask}>
+                                <FontAwesomeIcon icon="trash" />
+                            </span>
                         </div>
                     </div>
-                </div>
-                <div className="card-body" onClick={this.removeTask}>
-                    <span className="remove-task">
-                        <FontAwesomeIcon icon="times" />
-                    </span>
                 </div>
             </div>
         );
--- src/layouts/plugins/FontAwesome.js
+++ src/layouts/plugins/FontAwesome.js
@@ -1,7 +1,8 @@
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTimes } from '@fortawesome/free-solid-svg-icons'
+import { faTimes, faTrash } from '@fortawesome/free-solid-svg-icons'
 
 library.add(faTimes);
+library.add(faTrash);
 
 export default FontAwesomeIcon;
