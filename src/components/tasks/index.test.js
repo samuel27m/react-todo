6dfe27cd0e275641001297eded82a4462ef7a63c
Samuel Caçador <samuelmoreira27@gmail.com>
2019-04-05 18:04:30 +0100

    index.test.js DOC

--- src/components/tasks/index.test.js
+++ src/components/tasks/index.test.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from '.';
 
+/**
+ * TODO: Create tests for all components
+ */
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Index />, div);
